79690a115b89ad3d913ffe87f356557dcf753b72
李志钊 <lizhizhaooo@163.com>
2020-05-30 22:15:10 +0800

    [初始化]

---
+++ src/main/java/com/lizhizhao/bizBug/BizBugApplication.java
@@ -0,0 +1,13 @@
+package com.lizhizhao.bizBug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizBugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizBugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lizhizhao/bizBug/BizBugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lizhizhao.bizBug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizBugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
